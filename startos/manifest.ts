25d2c82f8737e25ba27836af4cd4d7dab42a1f90
aldum <aldum@artixlinux.org>
2025-12-04 10:56:01 +0100

    feat: import multiarch build

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -1,4 +1,10 @@
 import { setupManifest } from '@start9labs/start-sdk'
+import { SDKImageInputSpec } from '@start9labs/start-sdk/base/lib/types/ManifestTypes'
+
+const BUILD = process.env.BUILD || ''
+
+const architectures =
+  BUILD === 'x86_64' || BUILD === 'aarch64' ? [BUILD] : ['x86_64', 'aarch64']
 
 export const manifest = setupManifest({
   id: 'nextcloud',
@@ -16,12 +22,13 @@ export const manifest = setupManifest({
   volumes: ['main', 'nextcloud', 'db'],
   images: {
     nextcloud: {
+      arch: architectures,
       source: {
         dockerBuild: {},
       },
-    },
+    } as SDKImageInputSpec,
   },
-  hardwareRequirements: {},
+  hardwareRequirements: { arch: architectures },
   alerts: {
     install: null,
     update: null,
